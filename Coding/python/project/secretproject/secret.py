4149d18597e2348eaf4ad09114d71e382cec4f1d
umjiwan <bjwsuoer09@gmail.com>
2021-03-16 15:40:21 +0900

    secret.py -V 1 ( 복호화는 작업중 )

--- Coding/python/project/secretproject/secret.py
+++ Coding/python/project/secretproject/secret.py
@@ -233,7 +233,7 @@ mhh = "qioooqpriiijjdrfmnnhjkiz$"
 nhh = "dkfjioqjiouiofasdffhjdklmn"
 ohh = "uhjfdnj!@^-adsf="
 phh = "ulhaqiruiooidufiuoqasdfiuij"
-qhh = "iopfj!^*^*#&^&*aaaff!i"
+qhh = "iopasdffj!^*^*#&^&*aaaff!i"
 rhh = "*fjkqiiiiiqiflgglpjljlkjfn"
 shh = "suiiianfkpppjkqo"
 thh = "tryjnnnmdoola;ghfkokfo"
@@ -271,10 +271,16 @@ xii = "whdnqlsqhrhtlvsawefwsekwhsskwjdakffhalclehfhr"
 yii = "hldrlf;dehdsfdfqhrhtlvek"
 zii = "dkfjdsfsdfdskljiqiouuifjklwkldfjjsdmm,mxmkmzmkakjkjqopropoproproqihjfkmvmnjn"
 
-    
+print("무엇을 하시겠습니까?")
+print("----------------------")
+print("1. 암호화")
+print("2. 복호화")
+print("----------------------")
+choice = int(input("숫자만 입력 : "))
 
-        
-def password():
+if choice == 1:
+    print("암호화 프로그램 시작")
+    print("") 
     print("암호화하실 문장을 입력하여주세요.")
     password_str = input("")
     password_str_len = len(password_str)
@@ -284,30 +290,603 @@ def password():
     print(f"자릿수 : {password_str_len}")
     print(f"리스트 : {password_str_list}")
     print("")
-    
+
     print(type(password_str_len))
     
-
-    #while int(password_str_len) == start_nu:
-     #   print("안녕")
-      #  start_nu += 1
+    for count in password_str_list:
+        if count == "a":
+            a = random.randint(1, 10)
+            if a == 1:
+                print(a)
+            elif a == 2:
+                print(aaa)
+            elif a == 3:
+                print(abb)
+            elif a == 4:
+                print(acc)
+            elif a == 5:
+                print(add)
+            elif a == 6:
+                print(aee)
+            elif a == 7:
+                print(aff)
+            elif a == 8:
+                print(agg)
+            elif a == 9:
+                print(ahh)
+            elif a == 10:
+                print(aii)
+        elif count == "b":
+            random_nu = random.randint(1, 10)
+            if random_nu == 1:
+                print(b)
+            elif random_nu == 2:
+                print(baa)
+            elif random_nu == 3:
+                print(bbb)
+            elif random_nu == 4:
+                print(bcc)
+            elif random_nu == 5:
+                print(bdd)
+            elif random_nu == 6:
+                print(bee)
+            elif random_nu == 7:
+                print(bff)
+            elif random_nu == 8:
+                print(bgg)
+            elif random_nu == 9:
+                print(bhh)
+            elif random_nu == 10:
+                print(bii)
+        elif count == "c":
+            random_nu = random.randint(1, 10)
+            if random_nu == 1:
+                print(c)
+            elif random_nu == 2:
+                print(caa)
+            elif random_nu == 3:
+                print(cbb)
+            elif random_nu == 4:
+                print(ccc)
+            elif random_nu == 5:
+                print(cdd)
+            elif random_nu == 6:
+                print(cee)
+            elif random_nu == 7:
+                print(cff)
+            elif random_nu == 8:
+                print(cgg)
+            elif random_nu == 9:
+                print(chh)
+            elif random_nu == 10:
+                print(cii)
         
+        elif count == "d":
+            random_nu = random.randint(1, 10)
+            if random_nu == 1:
+                print(d)
+            elif random_nu == 2:
+                print(daa)
+            elif random_nu == 3:
+                print(dbb)
+            elif random_nu == 4:
+                print(dcc)
+            elif random_nu == 5:
+                print(ddd)
+            elif random_nu == 6:
+                print(dee)
+            elif random_nu == 7:
+                print(dff)
+            elif random_nu == 8:
+                print(dgg)
+            elif random_nu == 9:
+                print(dhh)
+            elif random_nu == 10:
+                print(dii)
+        
+        elif count == "e":
+            random_nu = random.randint(1, 10)
+            if random_nu == 1:
+                print(e)
+            elif random_nu == 2:
+                print(eaa)
+            elif random_nu == 3:
+                print(ebb)
+            elif random_nu == 4:
+                print(ecc)
+            elif random_nu == 5:
+                print(edd)
+            elif random_nu == 6:
+                print(eee)
+            elif random_nu == 7:
+                print(eff)
+            elif random_nu == 8:
+                print(egg)
+            elif random_nu == 9:
+                print(ehh)
+            elif random_nu == 10:
+                print(eii)
+        
+        elif count == "f":
+            random_nu = random.randint(1, 10)
+            if random_nu == 1:
+                print(f)
+            elif random_nu == 2:
+                print(faa)
+            elif random_nu == 3:
+                print(fbb)
+            elif random_nu == 4:
+                print(fcc)
+            elif random_nu == 5:
+                print(fdd)
+            elif random_nu == 6:
+                print(fee)
+            elif random_nu == 7:
+                print(fff)
+            elif random_nu == 8:
+                print(fgg)
+            elif random_nu == 9:
+                print(fhh)
+            elif random_nu == 10:
+                print(fii)
+        
+        elif count == "g":
+            random_nu = random.randint(1, 10)
+            if random_nu == 1:
+                print(g)
+            elif random_nu == 2:
+                print(gaa)
+            elif random_nu == 3:
+                print(gbb)
+            elif random_nu == 4:
+                print(gcc)
+            elif random_nu == 5:
+                print(gdd)
+            elif random_nu == 6:
+                print(gee)
+            elif random_nu == 7:
+                print(gff)
+            elif random_nu == 8:
+                print(ggg)
+            elif random_nu == 9:
+                print(ghh)
+            elif random_nu == 10:
+                print(gii)
+        
+        elif count == "h":
+            random_nu = random.randint(1, 10)
+            if random_nu == 1:
+                print(h)
+            elif random_nu == 2:
+                print(haa)
+            elif random_nu == 3:
+                print(hbb)
+            elif random_nu == 4:
+                print(hcc)
+            elif random_nu == 5:
+                print(hdd)
+            elif random_nu == 6:
+                print(hee)
+            elif random_nu == 7:
+                print(hff)
+            elif random_nu == 8:
+                print(hgg)
+            elif random_nu == 9:
+                print(hhh)
+            elif random_nu == 10:
+                print(hii)
+        
+        elif count == "i":
+            random_nu = random.randint(1, 10)
+            if random_nu == 1:
+                print(i)
+            elif random_nu == 2:
+                print(iaa)
+            elif random_nu == 3:
+                print(ibb)
+            elif random_nu == 4:
+                print(icc)
+            elif random_nu == 5:
+                print(idd)
+            elif random_nu == 6:
+                print(iee)
+            elif random_nu == 7:
+                print(iff)
+            elif random_nu == 8:
+                print(igg)
+            elif random_nu == 9:
+                print(ihh)
+            elif random_nu == 10:
+                print(iii)
+        
+        elif count == "j":
+            random_nu = random.randint(1, 10)
+            if random_nu == 1:
+                print(j)
+            elif random_nu == 2:
+                print(jaa)
+            elif random_nu == 3:
+                print(jbb)
+            elif random_nu == 4:
+                print(jcc)
+            elif random_nu == 5:
+                print(jdd)
+            elif random_nu == 6:
+                print(jee)
+            elif random_nu == 7:
+                print(jff)
+            elif random_nu == 8:
+                print(jgg)
+            elif random_nu == 9:
+                print(jhh)
+            elif random_nu == 10:
+                print(jii)
+        
+        elif count == "k":
+            random_nu = random.randint(1, 10)
+            if random_nu == 1:
+                print(k)
+            elif random_nu == 2:
+                print(kaa)
+            elif random_nu == 3:
+                print(kbb)
+            elif random_nu == 4:
+                print(kcc)
+            elif random_nu == 5:
+                print(kdd)
+            elif random_nu == 6:
+                print(kee)
+            elif random_nu == 7:
+                print(kff)
+            elif random_nu == 8:
+                print(kgg)
+            elif random_nu == 9:
+                print(khh)
+            elif random_nu == 10:
+                print(kii)
+        
+        elif count == "l":
+            random_nu = random.randint(1, 10)
+            if random_nu == 1:
+                print(l)
+            elif random_nu == 2:
+                print(laa)
+            elif random_nu == 3:
+                print(lbb)
+            elif random_nu == 4:
+                print(lcc)
+            elif random_nu == 5:
+                print(ldd)
+            elif random_nu == 6:
+                print(lee)
+            elif random_nu == 7:
+                print(lff)
+            elif random_nu == 8:
+                print(lgg)
+            elif random_nu == 9:
+                print(lhh)
+            elif random_nu == 10:
+                print(lii)
+        
+        elif count == "n":
+            random_nu = random.randint(1, 10)
+            if random_nu == 1:
+                print(n)
+            elif random_nu == 2:
+                print(naa)
+            elif random_nu == 3:
+                print(nbb)
+            elif random_nu == 4:
+                print(ncc)
+            elif random_nu == 5:
+                print(ndd)
+            elif random_nu == 6:
+                print(nee)
+            elif random_nu == 7:
+                print(nff)
+            elif random_nu == 8:
+                print(ngg)
+            elif random_nu == 9:
+                print(nhh)
+            elif random_nu == 10:
+                print(nii)
+        
+        elif count == "m":
+            random_nu = random.randint(1, 10)
+            if random_nu == 1:
+                print(m)
+            elif random_nu == 2:
+                print(maa)
+            elif random_nu == 3:
+                print(mbb)
+            elif random_nu == 4:
+                print(mcc)
+            elif random_nu == 5:
+                print(mdd)
+            elif random_nu == 6:
+                print(mee)
+            elif random_nu == 7:
+                print(mff)
+            elif random_nu == 8:
+                print(mgg)
+            elif random_nu == 9:
+                print(mhh)
+            elif random_nu == 10:
+                print(mii)
+        
+        elif count == "o":
+            random_nu = random.randint(1, 10)
+            if random == 1:
+                print(o)
+            elif random == 2:
+                print(oaa)
+            elif random == 3:
+                print(obb)
+            elif random == 4:
+                print(occ)
+            elif random == 5:
+                print(odd)
+            elif random == 6:
+                print(oee)
+            elif random == 7:
+                print(off)
+            elif random == 8:
+                print(ogg)
+            elif random == 9:
+                print(ohh)
+            elif random == 10:
+                print(oii)
+        
+        elif count == "p":
+            random = random.randint(1, 10)
+            if random == 1:
+                print(p)
+            elif random == 2:
+                print(paa)
+            elif random == 3:
+                print(pbb)
+            elif random == 4:
+                print(pcc)
+            elif random == 5:
+                print(pdd)
+            elif random == 6:
+                print(pee)
+            elif random == 7:
+                print(pff)
+            elif random == 8:
+                print(pgg)
+            elif random == 9:
+                print(phh)
+            elif random == 10:
+                print(pii)
+        
+        elif count == "q":
+            random_nu = random.randint(1, 10)
+            if random_nu == 1:
+                print(q)
+            elif random_nu == 2:
+                print(qaa)
+            elif random_nu == 3:
+                print(qbb)
+            elif random_nu == 4:
+                print(qcc)
+            elif random_nu == 5:
+                print(qdd)
+            elif random_nu == 6:
+                print(qee)
+            elif random_nu == 7:
+                print(qff)
+            elif random_nu == 8:
+                print(qgg)
+            elif random_nu == 9:
+                print(qhh)
+            elif random_nu == 10:
+                print(qii)
+        
+        elif count == "r":
+            random_nu = random.randint(1, 10)
+            if random_nu == 1:
+                print(r)
+            elif random_nu == 2:
+                print(raa)
+            elif random_nu == 3:
+                print(rbb)
+            elif random_nu == 4:
+                print(rcc)
+            elif random_nu == 5:
+                print(rdd)
+            elif random_nu == 6:
+                print(ree)
+            elif random_nu == 7:
+                print(rff)
+            elif random_nu == 8:
+                print(rgg)
+            elif random_nu == 9:
+                print(rhh)
+            elif random_nu == 10:
+                print(rii)
+        
+        elif count == "s":
+            random_nu = random.randint(1, 10)
+            if random_nu == 1:
+                print(s)
+            elif random_nu == 2:
+                print(saa)
+            elif random_nu == 3:
+                print(sbb)
+            elif random_nu == 4:
+                print(scc)
+            elif random_nu == 5:
+                print(sdd)
+            elif random_nu == 6:
+                print(see)
+            elif random_nu == 7:
+                print(sff)
+            elif random_nu == 8:
+                print(sgg)
+            elif random_nu == 9:
+                print(shh)
+            elif random_nu == 10:
+                print(sii)
+        
+        elif count == "t":
+            random_nu = random.randint(1, 10)
+            if random_nu == 1:
+                print(t)
+            elif random_nu == 2:
+                print(taa)
+            elif random_nu == 3:
+                print(tbb)
+            elif random_nu == 4:
+                print(tcc)
+            elif random_nu == 5:
+                print(tdd)
+            elif random_nu == 6:
+                print(tee)
+            elif random_nu == 7:
+                print(tff)
+            elif random_nu == 8:
+                print(tgg)
+            elif random_nu == 9:
+                print(thh)
+            elif random_nu == 10:
+                print(tii)
+        
+        elif count == "u":
+            random_nu = random.randint(1, 10)
+            if random_nu == 1:
+                print(u)
+            elif random_nu == 2:
+                print(uaa)
+            elif random_nu == 3:
+                print(ubb)
+            elif random_nu == 4:
+                print(ucc)
+            elif random_nu == 5:
+                print(udd)
+            elif random_nu == 6:
+                print(uee)
+            elif random_nu == 7:
+                print(uff)
+            elif random_nu == 8:
+                print(ugg)
+            elif random_nu == 9:
+                print(uhh)
+            elif random_nu == 10:
+                print(uii)
 
-
-
-def start():
-    print("무엇을 하시겠습니까?")
-    print("----------------------")
-    print("1. 암호화")
-    print("2. 복호화")
-    print("----------------------")
-    choice = int(input("숫자만 입력 : "))
-
-    if choice == 1:
-        print("암호화 프로그램 시작")
-        print("")
-        password()
-
-print("start")
-print("")
-start()
+        elif count == "v":
+            random_nu = random.randint(1, 10)
+            if random_nu == 1:
+                print(v)
+            elif random_nu == 2:
+                print(vaa)
+            elif random_nu == 3:
+                print(vbb)
+            elif random_nu == 4:
+                print(vcc)
+            elif random_nu == 5:
+                print(vdd)
+            elif random_nu == 6:
+                print(vee)
+            elif random_nu == 7:
+                print(vff)
+            elif random_nu == 8:
+                print(vgg)
+            elif random_nu == 9:
+                print(vhh)
+            elif random_nu == 10:
+                print(vii)
+        
+        elif count == "w":
+            random_nu = random.randint(1, 10)
+            if random_nu == 1:
+                print(w)
+            elif random_nu == 2:
+                print(waa)
+            elif random_nu == 3:
+                print(wbb)
+            elif random_nu == 4:
+                print(wcc)
+            elif random_nu == 5:
+                print(wdd)
+            elif random_nu == 6:
+                print(wee)
+            elif random_nu == 7:
+                print(wff)
+            elif random_nu == 8:
+                print(wgg)
+            elif random_nu == 9:
+                print(whh)
+            elif random_nu == 10:
+                print(wii)
+        
+        elif count == "x":
+            random_nu = random.randint(1, 10)
+            if random_nu == 1:
+                print(x)
+            elif random_nu == 2:
+                print(xaa)
+            elif random_nu == 3:
+                print(xbb)
+            elif random_nu == 4:
+                print(xcc)
+            elif random_nu == 5:
+                print(xdd)
+            elif random_nu == 6:
+                print(xee)
+            elif random_nu == 7:
+                print(xff)
+            elif random_nu == 8:
+                print(xgg)
+            elif random_nu == 9:
+                print(xhh)
+            elif random_nu == 10:
+                print(xii)
+        
+        elif count == "y":
+            random_nu = random.randint(1, 10)
+            if random_nu == 1:
+                print(y)
+            elif random_nu == 2:
+                print(yaa)
+            elif random_nu == 3:
+                print(ybb)
+            elif random_nu == 4:
+                print(ycc)
+            elif random_nu == 5:
+                print(ydd)
+            elif random_nu == 6:
+                print(yee)
+            elif random_nu == 7:
+                print(yff)
+            elif random_nu == 8:
+                print(ygg)
+            elif random_nu == 9:
+                print(yhh)
+            elif random_nu == 10:
+                print(yii)
+        
+        elif count == "z":
+            random_nu = random.randint(1, 10)
+            if random_nu == 1:
+                print(z)
+            elif random_nu == 2:
+                print(zaa)
+            elif random_nu == 3:
+                print(zbb)
+            elif random_nu == 4:
+                print(zcc)
+            elif random_nu == 5:
+                print(zdd)
+            elif random_nu == 6:
+                print(zee)
+            elif random_nu == 7:
+                print(zff)
+            elif random_nu == 8:
+                print(zgg)
+            elif random_nu == 9:
+                print(zhh)
+            elif random_nu == 10:
+                print(zii)
+    
